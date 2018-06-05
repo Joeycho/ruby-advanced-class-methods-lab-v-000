149dc7d7adb655bd634e9f540ae2eaa692af461f
Joey Cho <cjy12182000@naver.com>
2018-06-05 11:28:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,10 +21,13 @@ class Song
   def self.new_by_name(name)
     #binding.pry
     song = self.new
-
     song.name = name
-
     song
   end
 
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
 end
