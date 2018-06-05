e1d8e7ad659db87b1154b7d9fe2e5a5e1ec619a7
Joey Cho <cjy12182000@naver.com>
2018-06-05 11:41:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,4 +30,21 @@ class Song
     song.name = name
     @@all << song
     song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name != nil
+      self.create_by_name(name)
+    else
+      self.find_by_name
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by!{|s| s.name.downcase}
+  end
 end
